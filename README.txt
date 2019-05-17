d9552445ecde44651ea363ecc1773a0f8dece7e6
Kourpa <koopastroopa@gmail.com>
2019-05-16 22:58:02 -0500

    everything works

---
+++ README.txt
@@ -0,0 +1,16 @@
+- links
+    - http://localhost:8080/book-service/books
+    - http://localhost:8080/discovery
+    - http://localhost:8080/rating-service/ratings/all
+
+application-config is needed for various different properties. Update the path in the config server application.properties to reflect where you put this folder
+NOTE: In windows the path must start with "file:///" and in linux "file://"
+
+- User
+    - username: user
+    - password: password
+
+- Admin
+    - username: admin
+    - password: admin
+
--- discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -1,8 +1,15 @@
 package com.example.discovery;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.PostConstruct;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +17,12 @@ public class DiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
 	}
 
 }
